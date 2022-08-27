4680968240e5f8afa5f95cf55c79231380fc0c60
jadashi97 <njadauniversal23@yahoo.com>
2022-08-26 19:56:21 -0500

    (feat): moved the header into its own folder in components

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link, Outlet } from "react-router-dom";
-import  logo from "../pics/logo.png"
+// import  logo from "../pics/logo.png"
 
 // import './App.css';
 
 
 function Header(){
+
     return(
         <>
             <div className="header">
                 <nav>
                     {/* <img  alt="myimage" src={logo} ></img> */}
-                    <h1>SouthSudanese Community Church</h1>
+                    <h1>
+                        SouthSudanese 
+                        Community 
+                        Church</h1>
                     <div>
                         <Link to="/home">Home</Link> | {" "}
                         <Link to="/about">About us</Link> | {" "}
