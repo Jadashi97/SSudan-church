18a066ae4f2f7ee9395cfd7d8e2a097506dc88be
jadashi97 <njadauniversal23@yahoo.com>
2022-08-28 09:22:16 -0400

    (feat): redesigned the header. (fix): work on footer, and other pages

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -14,16 +14,20 @@ function Header(){
                 <nav>
                     {/* <img  alt="myimage" src={logo} ></img> */}
                     <div className="header">
-                        <h1>
-                            SouthSudanese 
-                            Community 
-                            Church
-                        </h1>   
-                        <Link to="/home">Home</Link> | {" "}
-                        <Link to="/about">About us</Link> | {" "}
-                        <Link to="/events">Events</Link> | {" "}
-                        <Link to="/sermons">Sermons & Teaching</Link> | {" "}
-                        <Link to="/Offering"><button>Offering</button></Link> | {" "}
+                        <div className="heading">
+                            <h1>SouthSudanese </h1>
+                            <h1>Community</h1>
+                            <h1>Church</h1> 
+                        </div>
+                            
+                        <div className="links">
+                            <Link to="/home">Home</Link> | {" "}
+                            <Link to="/about">About us</Link> | {" "}
+                            <Link to="/events">Events</Link> | {" "}
+                            <Link to="/sermons">Sermons & Teaching</Link> | {" "}
+                            <Link to="/Offering"><button>Offering</button></Link> | {" "}
+                        </div>
+                        
                     </div>
                 </nav>
                 <Outlet/>   
