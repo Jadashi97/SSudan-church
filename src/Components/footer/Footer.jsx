ab6fb5d2def25f18828f87a75ced2ca6ef239756
jadashi97 <njadauniversal23@yahoo.com>
2022-08-29 21:21:14 -0500

    (feat): added footer. (fix): work on footer, add some styles, and other pages

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 // import {Link, Outlet } from "react-router-dom";
+import "./footer.css"
 
 
 
 function Footer(){
     return(
-        <div >
-                        <div className="address">
-                            <p>715 fridley </p>
-                            <p>MN</p>
-                            <p>MN</p> 
-                         </div>
-                        <div className="footer-links">
-                            <a href="/home">Home</a> |
-                            <a href="/about">About us</a> |
-                            <a href="/events">Events</a> |
-                            <a href="/sermons">Sermons & Teaching</a> |
-                            <a href="/Offering"><button>Offering</button></a> |
-                        </div> 
+        <div>
+            <div className="footer">
+                <div className="address">
+                    <p>715 fridley </p>
+                    <p>MN</p>
+                    <p>MN</p> 
+                </div>
+                <div className="footer-links">
+                    <a href="/home">Home</a> |
+                    <a href="/about">About us</a> |
+                    <a href="/events">Events</a> |
+                    <a href="/sermons">Sermons & Teaching</a> |
+                    <a href="/Offering"><button>Offering</button></a> |
+                </div> 
+            </div>
         </div>
     )
 }
